c0781b2390bdaf4bdd9128b783f2ec37698e65ec
romety2 <romety2@wp.pl>
2017-08-03 00:23:58 +0200

    poprawiono wyszukiwarke

--- Pallets/App.cs
+++ Pallets/App.cs
@@ -50,6 +50,18 @@ namespace Pallets
             dataGridView1.AllowUserToAddRows = false;
         }
 
+        private void SearchEvents()
+        {
+            BindingSource events = new BindingSource();
+            int result = DateTime.Compare(DateTime.Parse(getEvents()[0].Date), dateTimePicker1.Value.Date);
+            events.DataSource = getEvents()
+                .Where(o => DateTime.Compare(DateTime.Parse(o.Date), dateTimePicker1.Value.Date) >= 0
+                && DateTime.Compare(DateTime.Parse(o.Date), dateTimePicker2.Value.Date) <= 0
+                && o.Company.Name.ToLower().Contains(textBox1.Text.ToLower()) == true)
+                .OrderBy(o => DateTime.Parse(o.Date)).ThenBy(t => t.Company.Name).ToList();
+            dataGridView1.DataSource = events;
+        }
+
         private void Management_Load(object sender, EventArgs e)
         {
             BindingSource events = new BindingSource();
@@ -70,14 +82,6 @@ namespace Pallets
             name.Show();
         }
 
-        private void CheckDateRanges()
-        {
-            BindingSource events = new BindingSource();
-            int result = DateTime.Compare(DateTime.Parse(getEvents()[0].Date), dateTimePicker1.Value.Date);
-            events.DataSource = getEvents().Where(o => DateTime.Compare(DateTime.Parse(o.Date), dateTimePicker1.Value.Date) >= 0 && DateTime.Compare(DateTime.Parse(o.Date), dateTimePicker2.Value.Date) <= 0).OrderBy(o => DateTime.Parse(o.Date)).ThenBy(t => t.Company.Name).ToList();
-            dataGridView1.DataSource = events;
-        }
-
         public void addData(string data2, BindingList<Event.MyPalette> mpallets, Company company, string comment)
         {
             int id;
@@ -87,7 +91,9 @@ namespace Pallets
                 id = dataGridView1.CurrentCellAddress.Y;
             saved = false;
             BindingSource w = new BindingSource();
-            w.DataSource = dc.addEvent(data2, mpallets, company, comment).Where(o => o.Date.Equals(dateTimePicker1.Value.ToShortDateString()) == true).OrderBy(o => DateTime.Parse(o.Date)).ThenBy(t => t.Company.Name);
+            w.DataSource = dc.addEvent(data2, mpallets, company, comment)
+                .Where(o => o.Date.Equals(dateTimePicker1.Value.ToShortDateString()) == true)
+                .OrderBy(o => DateTime.Parse(o.Date)).ThenBy(t => t.Company.Name);
             dataGridView1.DataSource = w;
             if (dataGridView1.Rows.Count != 1 && textBox1.Text == "")
                 dataGridView1.CurrentCell = dataGridView1[2, id];
@@ -184,19 +190,17 @@ namespace Pallets
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            BindingSource bs = new BindingSource();
-            bs.DataSource = getEvents().Where(o => o.Company.Name.ToLower().Contains(textBox1.Text.ToLower()) == true).OrderBy(o => DateTime.Parse(o.Date)).ThenBy(t => t.Company.Name).ToList();
-            dataGridView1.DataSource = bs; dataGridView1.DataSource = bs;
+            SearchEvents();
         }
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
-            CheckDateRanges();
+            SearchEvents();
         }
 
         private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
         {
-            CheckDateRanges();
+            SearchEvents();
         }
     }
 }
